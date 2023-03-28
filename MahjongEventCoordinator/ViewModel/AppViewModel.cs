2065168b3667e3a55d74c0dc4503deceac20cf70
Shane Zamora <readydesigncorps@gmail.com>
2023-03-28 02:13:52 -0700

    Push changes.

--- MahjongEventCoordinator/ViewModel/AppViewModel.cs
+++ MahjongEventCoordinator/ViewModel/AppViewModel.cs
@@ -1,10 +1,13 @@
 ﻿// [Ready Design Corps] - [Mahjong Event Coordinator] - Copyright 2023
 
 using MahjongEventCoordinator.Common;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Windows.Data;
 
 namespace MahjongEventCoordinator.ViewModel
 {
-    public class AppViewModel
+    public class AppViewModel : BindableBase
     {
         public int             RoundCount                { get => 4;                        set { } }
         public int             UpperDivisionModifier     { get => 4;                        set { } }
@@ -12,6 +15,16 @@ namespace MahjongEventCoordinator.ViewModel
         public int             ChomboPenalty             { get => 20;                       set { } }
         public int             UpperDivisionTableCount   { get => 1;                        set { } }
         public SeatingStrategy FinalRoundSeatingStrategy { get => SeatingStrategy.HzMethod; set { } }
+        public ICollectionView TournamentPages           { get;                             private set; }
 
+        private List<BindableBase> _Pages = new List<BindableBase>();
+
+        public AppViewModel()
+        {
+            // Initialize collection of pages. For now we will point to ourselves
+            // as the 'base' page, which will represent the settings page in the UX.
+            _Pages.Add(this);
+            TournamentPages = CollectionViewSource.GetDefaultView(_Pages);
+        }
     }
 }
---
+++ MahjongEventCoordinator/ViewModel/BindableBase.cs
@@ -0,0 +1,37 @@
+﻿// [Ready Design Corps] - [Mahjong Event Coordinator] - Copyright 2023
+
+using System;
+using System.ComponentModel;
+
+namespace MahjongEventCoordinator.ViewModel
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void Assign<T>(string propertyName, out T storage, T existing, T value) where T : IComparable
+        {
+            if ((value == null) || (value.CompareTo(existing) != 0))
+            {
+                storage = value;
+                Notify(propertyName);
+            }
+            else
+            {
+                storage = value;
+            }
+        }
+
+        public void Notify(string propertyName) { PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName)); }
+
+        public void Notify(string p1, string p2, string p3 = null, string p4 = null, string p5 = null, string p6 = null)
+        {
+            Notify(p1);
+            Notify(p2);
+            if (p3 != null) { Notify(p3); }
+            if (p4 != null) { Notify(p4); }
+            if (p5 != null) { Notify(p5); }
+            if (p6 != null) { Notify(p6); }
+        }
+    }
+}
