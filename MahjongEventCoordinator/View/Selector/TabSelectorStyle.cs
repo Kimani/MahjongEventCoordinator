6542f9af7e8d647813bb6c5c8ccbe14c1d2ca370
Shane Zamora <readydesigncorps@gmail.com>
2023-11-17 01:57:06 -0800

    Some other changes

---
+++ MahjongEventCoordinator/View/Selector/TabSelectorStyle.cs
@@ -0,0 +1,23 @@
+﻿// [Ready Design Corps] - [Mahjong Event Coordinator] - Copyright 2023
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace MahjongEventCoordinator.View.Selector
+{
+    public class TabSelectorStyle : DataTemplateSelector
+    {
+        public DataTemplate SetupTemplate { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return (item != null) ? SetupTemplate : null;
+        }
+
+    }
+}
